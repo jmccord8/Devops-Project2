934d6dc1f900664f42b9e9771e33b6ea28297e6b
Jordan McCord <jmccord8@my.westga.edu>
2024-04-11 15:29:50 -0400

    Completed test cases for ItemManager class.

--- code/Devops-Project2/src/main/java/edu/westga/devops/project2/model/ItemManager.java
+++ code/Devops-Project2/src/main/java/edu/westga/devops/project2/model/ItemManager.java
@@ -1,6 +1,5 @@
 package edu.westga.devops.project2.model;
 
-//import edu.westga.comp4420.javafx_sample.model.Note;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -14,5 +13,23 @@ public class ItemManager {
 
 	public ObservableList<Item> getItems() {
 		return this.items;
+	}
+
+	public void addItem(Item item) {
+		if (item == null) {
+			throw new IllegalArgumentException(NULL_ITEM);
+		}
+		this.items.add(item);
+	}
+
+	public void removeItem(Item item) {
+		if (item == null) {
+			throw new IllegalArgumentException(NULL_ITEM);
+		}
+		this.items.remove(item);
+	}
+
+	public int getSize() {
+		return this.items.size();
 	}
 }
